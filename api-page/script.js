85c605cc55a2aeb6d3d309147fcfb4ba4d532351
zyrä <okeeyaaa967@gmail.com>
2026-05-19 12:56:55 +0700

    Update script.js

--- api-page/script.js
+++ api-page/script.js
@@ -375,7 +375,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         
         if (DOM.apiLinksContainer) {
             DOM.apiLinksContainer.innerHTML = ''; 
-            const defaultLinks = [{ url: "https://github.com/zyraa-kageen/profile", name: "Lihat di GitHub", icon: "fab fa-github" }];
+            const defaultLinks = [{ url: "https://github.com/zyraa-kageen/", name: "Lihat di GitHub", icon: "fab fa-github" }];
             const linksToRender = settings.links?.length ? settings.links : defaultLinks;
 
             linksToRender.forEach(({ url, name, icon }, index) => {
