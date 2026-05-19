2da070b8df38e652821b807449d947ddc51dc780
zyrä <okeeyaaa967@gmail.com>
2026-05-19 13:50:47 +0700

    Update anime.js

--- src/api/anime/anime.js
+++ src/api/anime/anime.js
@@ -4,13 +4,13 @@ const baseURL = 'https://www.sankavollerei.com/anime';
 
 async function fetchJson(url) {
     try {
-        const { animenya } = await axios.get(url, {
+        const { data } = await axios.get(url, {
             headers: {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
         });
         // buang field creator/source, ambil data aja
-        const { creator, source, author, credit, statusCode, statusMessage, message, status, ok,...clean } = animenya;
+        const { creator, source, author, credit, statusCode, statusMessage, message, status, ok,...clean } = data;
         return clean;
     } catch (error) {
         console.error(`Error fetching ${url}:`, error);
