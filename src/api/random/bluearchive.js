426e120d7f7303bff28ca6d6930132ee001843ac
zyrä <okeeyaaa967@gmail.com>
2026-05-21 12:59:24 +0700

    Update bluearchive.js

--- src/api/random/bluearchive.js
+++ src/api/random/bluearchive.js
@@ -1,24 +1,28 @@
-const axios = require('axios');
-module.exports = function(app) {
-    async function bluearchive() {
-        try {
-            const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`)
-            const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
-            return Buffer.from(response.data);
-        } catch (error) {
-            throw error;
-        }
+import axios from 'axios';
+
+async function bluearchive() {
+    try {
+        const { data } = await axios.get(`https://raw.githubusercontent.com/rynxzyy/blue-archive-r-img/refs/heads/main/links.json`);
+        const response = await axios.get(data[Math.floor(data.length * Math.random())], { responseType: 'arraybuffer' });
+        return Buffer.from(response.data);
+    } catch (error) {
+        throw error;
     }
+}
+
+export default function(app) {
+
     app.get('/random/ba', async (req, res) => {
         try {
-            const pedo = await bluearchive();
+            const img = await bluearchive();
             res.writeHead(200, {
                 'Content-Type': 'image/png',
-                'Content-Length': pedo.length,
+                'Content-Length': img.length
             });
-            res.end(pedo);
+            res.end(img);
         } catch (error) {
             res.status(500).send(`Error: ${error.message}`);
         }
     });
-};
+
+}
