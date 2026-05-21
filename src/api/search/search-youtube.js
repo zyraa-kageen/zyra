905adb97c48c58db4ee339b18a13b2be7cd031c6
zyrä <okeeyaaa967@gmail.com>
2026-05-21 13:01:46 +0700

    Update search-youtube.js

--- src/api/search/search-youtube.js
+++ src/api/search/search-youtube.js
@@ -1,10 +1,10 @@
-module.exports = function(app) {
-    const yts = require('yt-search');
+import yts from 'yt-search';
+
+export default function(app) {
+
     app.get('/search/youtube', async (req, res) => {
         const { q } = req.query;
-        if (!q) {
-            return res.status(400).json({ status: false, error: 'Query is required' });
-        }
+        if (!q) return res.status(400).json({ status: false, error: 'Query is required' });
         try {
             const ytResults = await yts.search(q);
             const ytTracks = ytResults.videos.map(video => ({
@@ -14,12 +14,10 @@ module.exports = function(app) {
                 imageUrl: video.thumbnail,
                 link: video.url
             }));
-            res.status(200).json({
-                status: true,
-                result: ytTracks
-            });
+            res.status(200).json({ status: true, result: ytTracks });
         } catch (error) {
             res.status(500).json({ status: false, error: error.message });
         }
     });
+
 }
